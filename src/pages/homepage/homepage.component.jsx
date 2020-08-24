b60741960d0cdc362a107e7de84e6d7539d45a81
johnmorris4 <johnmorris4@bellsouth.net>
2020-08-24 06:34:36 -0400

    Added addition styles to the project. Closes #1

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DirectoryComponent from "../../components/directory/directory.component";
+import './homepage.styles.scss';
+
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <DirectoryComponent/>
+    </div>
+)
+
+
+export default HomePage;
